b0ebb5cdda65c3e6989ec66db8275d28a7f2b786
redacted-moose <sadlercr@rose-hulman.edu>
2014-02-06 16:13:42 -0500

    Comments and regionprops

--- src/features/getFeatureMatrix.m
+++ src/features/getFeatureMatrix.m
@@ -1,10 +1,24 @@
 function features = getFeatureMatrix(imgSeg)
-   % Operates on a segment of a bw image %
+   % Operates on a segment of a bw image
+   % By offset:
+   % Circularity by P^2/A
+   % Circularity by sigma/mu
+   % Row skew
+   % Column skew
+   % Row kurtosis
+   % Column kurtosis
+   % Elongation
+   % Percent fill of region
+   % Row size
+   % Column size
+   % Regionprops
+   stats = regionprops(imgSeg);
    features(1:2) = circularity(imgSeg);
    features(3:4) = skew(imgSeg);
    features(5:6) = kurtosis(imgSeg);
    features(7) = elongation(imgSeg);
-   features(8:9) = centroid(imgSeg);
-   features(10) = percentFill(imgSeg);
-   features(11:12) = size(imgSeg);
+%    features(8:9) = centroid(imgSeg);
+   features(8) = percentFill(imgSeg);
+   features(9:10) = size(imgSeg);
+   features = [features, stats];
 end
