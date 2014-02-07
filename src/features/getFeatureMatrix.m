68b5f8a609513459b2a32dc52a4c1d09a3afc327
redacted-moose <sadlercr@rose-hulman.edu>
2014-02-07 14:05:39 -0500

    Fixes in getFeatureMatrix.m

--- src/features/getFeatureMatrix.m
+++ src/features/getFeatureMatrix.m
@@ -16,6 +16,7 @@ function features = getFeatureMatrix(imgSeg)
        imgSeg = imread(imgSeg);
    end
    stats = regionprops(imgSeg);
+   stats
    features(1:2) = circularity(imgSeg);
    features(3:4) = skew(imgSeg);
    features(5:6) = kurtosis(imgSeg);
@@ -23,14 +24,14 @@ function features = getFeatureMatrix(imgSeg)
 %    features(8:9) = centroid(imgSeg);
    [r, c] = size(imgSeg);
    features(8:9) = [r, c];
-   features(10) = percentFill(imgSeg(1:r/3, 1:c/3));
-   features(11) = percentFill(imgSeg(r/3:2*r/3, 1:c/3));
-   features(12) = percentFill(imgSeg(2*r/3:r, 1:c/3));
-   features(13) = percentFill(imgSeg(1:r/3, c/3:2*c/3));
-   features(14) = percentFill(imgSeg(r/3:2*r/3, c/3:2*c/3));
-   features(15) = percentFill(imgSeg(2*r/3:r, c/3:2*c/3));
-   features(16) = percentFill(imgSeg(1:r/3, 2*c/3:c));
-   features(17) = percentFill(imgSeg(r/3:2*r/3, 2*c/3:c));
-   features(18) = percentFill(imgSeg(2*r/3:r, 2*c/3:c));
+   features(10) = percentFill(imgSeg(1:fix(r/3), 1:fix(c/3)));
+   features(11) = percentFill(imgSeg(fix(r/3):fix(2*r/3), 1:fix(c/3)));
+   features(12) = percentFill(imgSeg(fix(2*r/3):r, 1:fix(c/3)));
+   features(13) = percentFill(imgSeg(1:fix(r/3), fix(c/3):fix(2*c/3)));
+   features(14) = percentFill(imgSeg(fix(r/3):fix(2*r/3), fix(c/3):fix(2*c/3)));
+   features(15) = percentFill(imgSeg(fix(2*r/3):r, fix(c/3):fix(2*c/3)));
+   features(16) = percentFill(imgSeg(1:fix(r/3), fix(2*c/3):c));
+   features(17) = percentFill(imgSeg(fix(r/3):fix(2*r/3), fix(2*c/3):c));
+   features(18) = percentFill(imgSeg(fix(2*r/3):r, fix(2*c/3):c));
    features = [features];
 end
