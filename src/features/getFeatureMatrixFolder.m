a4131468fd9b62a672dc3af1b167f2c7ebac1309
redacted-moose <sadlercr@rose-hulman.edu>
2014-02-08 19:42:12 -0500

    Fix set stuff, but still has errors.

--- src/features/getFeatureMatrixFolder.m
+++ src/features/getFeatureMatrixFolder.m
@@ -1,14 +1,27 @@
 function [fm, im] = getFeatureMatrixFolder(filepath)
-    lookup = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', ...
-        'seven', 'eight', 'nine', 'addition', 'subtraction', ...
-        'multiplication', 'division', 'equals'; ...
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+    filepath
+    keyset = {'zero', 'one', 'two', 'three', 'four', 'five', 'six', ...
+                'seven', 'eight', 'nine', 'addition', 'subtraction', ...
+                'multiplication', 'division', 'equals'};
+    valueset = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+    lookup = containers.Map(keyset, valueset)
     files = dir(filepath);
     fm = zeros(numel(files));
-    k = findstr(filepath, lookup(1, :));
-    num = lookup(2, k);
-    for i=1:numel(files)
-        fm(i,:) = getFeatureMatrix(files(i));
-        im(i,1) = num;
+    for i=1:numel(keyset)
+        key = keyset(i)
+%         str = findstr(filepath, key); % This doesn't work for some reason
+        str = findstr(filepath, 'four');
+        if str
+            k = 'four';
+            k
+        end
+    end
+    
+    num = lookup(k);
+    for i=3:numel(files)
+        files(i)
+        img = imread([filepath, '\', files(i).name]);
+        fm(i, :) = getFeatureMatrix(img); % neither does this
+        im(i, 1) = num;
     end
 end
