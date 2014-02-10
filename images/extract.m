7a59a7b070722f7a1cc2fafa6463ad1e6f8c172c
davidMcneil <mcneil.david2@gmail.com>
2014-02-10 11:19:05 -0500

    added testing images

--- images/extract.m
+++ images/extract.m
@@ -1,20 +1,51 @@
 clear;
-load('2k2k.mat');
 addpath('../src/image_segmentation');
-
 W = 28;
 H = 28;
-numPerLine = 10;
 
-path = 'training/';
+% load('10kTrain.mat');
+% fea = full(fea);
+% path = 'training/';
+% 
+% for i=1:numel(fea)
+%     switch gnd(i)
+%     case 0
+%         dir = 'zero/';
+%     case 1
+%         dir = 'one/';
+%     case 2
+%         dir = 'two/';
+%     case 3
+%         dir = 'three/';
+%     case 4
+%         dir = 'four/';
+%     case 5
+%         dir = 'five/';
+%     case 6
+%         dir = 'six/';
+%     case 7
+%         dir = 'seven/';
+%     case 8
+%         dir = 'eight/';
+%     case 9
+%         dir = 'nine/'; 
+%     end
+%     syms = findSymbols(im2bw(reshape(fea(i,:),[H,W])'));
+%     % Find the most predominent (largest) symbol and save it
+%     [maxsize, maxidx]= max(cellfun(@numel,syms));
+%     imwrite(syms{maxidx}, [path dir int2str(i) '.png']);
+% end
 
-for i=1:numel(trainIdx)
-    
-    switch gnd(trainIdx(i))
+load('10kTest.mat');
+fea = full(fea);
+path = 'testing/';
+
+for i=1:numel(fea)
+    switch gnd(i)
     case 0
         dir = 'zero/';
     case 1
-        dir = 'one/';cd
+        dir = 'one/';
     case 2
         dir = 'two/';
     case 3
@@ -32,7 +63,7 @@ for i=1:numel(trainIdx)
     case 9
         dir = 'nine/'; 
     end
-    syms = findSymbols(im2bw(reshape(fea(trainIdx(i),:),[H,W])'));
+    syms = findSymbols(im2bw(reshape(fea(i,:),[H,W])'));
     % Find the most predominent (largest) symbol and save it
     [maxsize, maxidx]= max(cellfun(@numel,syms));
     imwrite(syms{maxidx}, [path dir int2str(i) '.png']);
