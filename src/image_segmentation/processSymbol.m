607b9b8d16bc38757ee6aa28eaa2cc24810d6ab3
PrinceWzz <princez.wzz@gmail.com>
2014-02-15 13:26:42 -0500

    add processSymbol

---
+++ src/image_segmentation/processSymbol.m
@@ -0,0 +1,12 @@
+function [image] = processSymbol(img)
+
+img_size = size(img,1)*size(img,2);
+element = strel('square',3);
+
+while length(find(img==1))/img_size < 0.5
+   
+    img = imdilate(img,element);
+    
+end
+    
+image = imresize(img,[25,25]);
