95d353cd3e6a5176553286669bddef4264350fcf
davidMcneil <mcneil.david2@gmail.com>
2014-02-15 23:31:28 -0500

    WORKING demo

--- src/convert.m
+++ src/convert.m
@@ -5,12 +5,15 @@ function res = convert(img)
        img = imread(img);
     end
     
+    keySet = {0, 1, 2, 3, 4, 5,  6, 7, 8, 9, 10, 11, 12, 13, 14};
+    valueSet = {'0', '1', '2', '3', '4', '5',  '6', '7', '8', '9', '+', '-', '*', '/', '='};
+    L = containers.Map(keySet,valueSet);
+    
     symbols = findSymbols(img);
     
     res = '';
     for i=1:numel(symbols)
         imtool(symbols{i});
-        res = [res ', ' int2str(classifySymbol(symbols{i}))];
+        res = [res L(classifySymbol(symbols{i}))];
     end
-    
 end
--- src/image_segmentation/findSymbols.m
+++ src/image_segmentation/findSymbols.m
@@ -18,7 +18,7 @@ function [symbols] = findSymbols(img)
     [BB, N] = findBoundingBoxes(BW);
     symbols = cell(N, 1); % Used to store image symbols
     for i=1:N
-        symbols{i} = (imcrop(BW, BB(i,:))); % Crop and save images
+        symbols{i} = processSymbol(imcrop(BW, BB(i,:))); % Crop and save images
 %         show(symbols{i}, 1);
 %         imwrite(symbols{i}, ['results/', 'new_' int2str(i), '.png']);
     end    
